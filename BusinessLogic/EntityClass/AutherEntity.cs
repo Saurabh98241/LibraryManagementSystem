b19a67c3641c787604fa298168d7cac6f83b0629
Saurabh98241 <46930335+Saurabh98241@users.noreply.github.com>
2019-11-28 14:04:02 -0500

    Update AutherEntity.cs

--- BusinessLogic/EntityClass/AutherEntity.cs
+++ BusinessLogic/EntityClass/AutherEntity.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace BusinessLogic.EntityClass
 {
     public class AuthorEntity
-    {//tttttttt
+    {//ttttttttrrrrr
         public int AuthorId { get; set; }
         public string AutherName { get; set; }
         public string AutherEmail { get; set; }
